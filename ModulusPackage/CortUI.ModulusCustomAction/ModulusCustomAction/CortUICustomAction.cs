91d787470d79b7ec8cd61c0271230aa6c26ebe44
Henrik Heimbuerger <henrik@heimbuerger.de>
2010-05-15 17:19:10 +0000

    [Partial fix for issue #26] Fixed cancelling of the installation from the custom action.
    The message when the cortui_settings.mdl can't be found during uninstallation has been removed.
    
    Also cleaned up the statusbar messages.

--- ModulusPackage/CortUI.ModulusCustomAction/ModulusCustomAction/CortUICustomAction.cs
+++ ModulusPackage/CortUI.ModulusCustomAction/ModulusCustomAction/CortUICustomAction.cs
@@ -42,11 +42,9 @@ namespace CortUI.ModulusCustomAction
             CustomizationForm customizationForm = new CustomizationForm();
             DialogResult result = customizationForm.ShowDialog();
 
-            Message = "not sure what to set after installation";
-
             if (result == DialogResult.OK)
             {
-                string pathToCortUiSettingsMdl = destinationPath + @"\artwork\cortui_settings.mdl";
+                string pathToCortUiSettingsMdl = Path.Combine(destinationPath, "artwork", "cortui_settings.mdl");
 
                 string settingsMdl;
                 try
@@ -58,7 +56,8 @@ namespace CortUI.ModulusCustomAction
                 }
                 catch
                 {
-                    MessageBox.Show("failed to read cortui_settings.mdl template");
+                    MessageBox.Show("Failed to read cortui_settings.mdl template, installation aborted.");
+                    Message = "Failed to read cortui_settings.mdl template, installation aborted.";
                     return false;
                 }
 
@@ -81,7 +80,9 @@ namespace CortUI.ModulusCustomAction
                 }
                 catch
                 {
-                    MessageBox.Show("failed to write cortui_settings.mdl template");
+                    MessageBox.Show("Failed to write cortui_settings.mdl.");
+                    Message = "Failed to write cortui_settings.mdl, installation aborted.";
+                    Cancelled = true;
                     return false;
                 }
 
@@ -89,22 +90,26 @@ namespace CortUI.ModulusCustomAction
             }
             else
             {
+                Message = "Installation aborted by user.";
+                Cancelled = true;
                 return false;
             }
         }
 
         public bool Uninstall(string destinationPath, string backupPath, IEnumerable<string> backedUpFiles)
         {
-            string pathToCortUiSettingsMdl = destinationPath + @"\artwork\cortui_settings.mdl";
+            string pathToCortUiSettingsMdl = Path.Combine(destinationPath, "artwork", "cortui_settings.mdl");
 
             if (!File.Exists(pathToCortUiSettingsMdl))
-                MessageBox.Show("Could not find " + pathToCortUiSettingsMdl);
+            {
+                //Message = "Could not find " + pathToCortUiSettingsMdl;
+            }
             else
+            {
                 File.Delete(pathToCortUiSettingsMdl);
+            }
 
-            Message = "not sure what to set after uninstallation";
-
-            return true;
+            return true; // proceed uninstallation even if cortui_settings.mdl has not been found
         }
     }
 }
